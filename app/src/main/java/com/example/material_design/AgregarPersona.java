6f71165a3dff605d6dc4f7a5e83bbe44c4c18453
jesus callejas <david.callejasc@hotmail.es>
2020-06-07 17:46:07 -0500

    Conexion firebase ok

--- app/src/main/java/com/example/material_design/AgregarPersona.java
+++ app/src/main/java/com/example/material_design/AgregarPersona.java
@@ -2,6 +2,7 @@ package com.example.material_design;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class AgregarPersona extends AppCompatActivity {
@@ -10,5 +11,12 @@ public class AgregarPersona extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_persona);
+    }
+
+
+    public void onBackPressed(){
+        //finish();
+        Intent i= new Intent(AgregarPersona.this,MainActivity.class);
+        startActivity(i);
     }
 }
